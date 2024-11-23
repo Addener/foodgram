05d2c80a7613e2061a5e3902361d848d68350555
Alecsei Uzhva <stas.basov@whatever.com>
2024-11-23 13:17:21 +0300

    edited users/admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
 from users.models import Follow, FoodgramUser
-
-User = get_user_model()
 
 
 admin.site.unregister(Group)
